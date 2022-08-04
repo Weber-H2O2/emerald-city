92418fabe303f3ba726c2166ed315a894ddf145b
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2022-08-04 23:08:57 +0800

    wip: Run round1

--- index.js
+++ index.js
@@ -10,25 +10,34 @@ async function keygen(m, arg) {
   return await m.gg18_keygen(t, n);
 }
 
+async function keygen_new_context(m, arg) {
+  return await m.gg18_keygen_client_context(t, n);
+}
+
+async function keygen_client_round1(m, context) {
+  return await m.gg18_keygen_client_round1(context);
+}
+
 async function sign(m, arg, key_store) {
   return await m.gg18_sign(t, n, key_store, "Hello Eigen");
 }
 
 thsig.then((m) => {
   items.forEach(async function (item) {
-    res = await keygen(m, item);
-    // console.log(item.idx, " ", res);
-    results.push(res);
+    context = await keygen_new_context(m);
+    console.log(context);
 
-    if (results.length == items.length) {
-      console.log(results.length);
-      items.forEach(async function (item) {
-        if (item.idx < t + 1) {
-          console.log(item.idx, " ", results[item.idx]);
-          res = await sign(m, item, results[item.idx]);
-          console.log("Sign result: ", res);
-        }
-      });
-    }
+    res = await keygen_client_round1(m, context);
+    results.push(res);
+    // if (results.length == items.length) {
+    //   console.log(results.length);
+    //   items.forEach(async function (item) {
+    //     if (item.idx < t + 1) {
+    //       console.log(item.idx, " ", results[item.idx]);
+    //       res = await sign(m, item, results[item.idx]);
+    //       console.log("Sign result: ", res);
+    //     }
+    //   });
+    // }
   });
 });
